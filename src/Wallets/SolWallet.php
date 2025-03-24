c005a842257408489665100f90ca6330ed5d7235
mitoop <me@mitoop.com>
2025-03-24 09:25:46 +0800

    go

---
+++ src/Wallets/SolWallet.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mitoop\Crypto\Wallets;
+
+use SodiumException;
+use StephenHill\Base58;
+use Throwable;
+
+class SolWallet implements WalletInterface
+{
+    /**
+     * @throws SodiumException
+     */
+    public function generate(): Wallet
+    {
+        $keypair = sodium_crypto_sign_keypair();
+        $privateKey = sodium_crypto_sign_secretkey($keypair);
+        $publicKey = sodium_crypto_sign_publickey($keypair);
+
+        $privateKey = (new Base58)->encode($privateKey);
+        $publicKey = (new Base58)->encode($publicKey);
+
+        return new Wallet($publicKey, $privateKey, $publicKey);
+    }
+
+    public function validate(string $address): bool
+    {
+        if (strlen($address) != 44) {
+            return false;
+        }
+
+        try {
+            return strlen((new Base58)->decode($address)) === 32;
+        } catch (Throwable) {
+            return false;
+        }
+    }
+}
