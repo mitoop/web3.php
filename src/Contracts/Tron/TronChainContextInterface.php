d8f104bc72420d9e47fd1679b0f4f81382c742f4
mitoop <me@mitoop.com>
2025-07-16 13:07:21 +0800

    add tron method
    Signed-off-by: mitoop <me@mitoop.com>

--- src/Contracts/Tron/TronChainContextInterface.php
+++ src/Contracts/Tron/TronChainContextInterface.php
@@ -15,4 +15,16 @@ interface TronChainContextInterface
     public function delegate(string $from, string $fromPrivateKey, string $to, $amount, Resource $resource): string;
 
     public function unDelegate(string $from, string $fromPrivateKey, string $to, $amount, Resource $resource): string;
+
+    public function getBandwidthPrices(): array;
+
+    public function getBandwidthPrice(): string;
+
+    public function setBandwidthPrice(string $price): static;
+
+    public function getEnergyPrices(): array;
+
+    public function getEnergyPrice(): string;
+
+    public function setEnergyPrice(string $price): static;
 }
